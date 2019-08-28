aaa507d30d4bf7d13e5dac680fd313e46870e12b
RichardLiao <bnmghjtyu0@gmail.com>
2019-08-28 02:25:36 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './button/index'
 
+
+const datas = [
+    {
+        id: 1,
+        name: 'Richard'
+    }, {
+        id: 2,
+        name: 'Kelly'
+    }
+]
+
 function App() {
     return (
         <div className="App">
-            <Button />
+            {
+                datas.map((data, idx) => {
+                    return (
+                        <Button datas={datas} data={data} />
+                    )
+                })
+            }
         </div>
     );
 }
--- src/button/index.js
+++ src/button/index.js
@@ -1,13 +1,41 @@
 import React, { useState } from 'react'
-const Button = () => {
-    const [isClick, setIsClick] = useState(false)
-    const toggle = () => {
-        setIsClick(!isClick)
+const Button = (props) => {
+    const [form, setForm] = useState({ name: '' } || props.data)
+    const [datas, setDatas] = useState([])
+    const [toggle, setToggle] = useState(false)
+    const postData = (data) => {
+        setToggle(!toggle)
+        setDatas(data)
+        setForm({
+            ...form,
+            name: ''
+        })
+    }
+    const evModify = () => {
+        console.log(form)
+        setToggle(!toggle)
+    }
+
+    const evToggle = () => {
+        setToggle(!toggle)
+        setForm({
+            ...form,
+            name: ''
+        })
     }
     return (
         <div>
-            <button data-testid="btn-toggle" onClick={toggle}>按鈕</button>
-            <div data-testid="content">{isClick ? 'clicked' : 'unclick'}</div>
+            {toggle ?
+                <div>
+                    <input type="text" name="name" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })} />
+                    <button data-testid="btn-toggle" onClick={evModify}>要修改</button>
+                    <button data-testid="btn-toggle" onClick={evToggle}>不修改</button>
+                </div> :
+                <div>
+                    <button data-testid="btn-toggle" onClick={e => postData(e, props.data)}>修改</button>
+                    <span>{form.name || props.data.name}</span>
+                </div>
+            }
         </div>
     )
 }
