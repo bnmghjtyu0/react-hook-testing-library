a131465a0c1f770519d235625fe566d725a369a2
RichardLiao <bnmghjtyu0@gmail.com>
2019-09-06 08:39:21 +0000

    update

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 |key|描述|
 | ---| --- |
-| getByLabelText |取 label 的文字re文字 |
-| getByLabelText | 取 label 的文字re文字 |
+| getByLabelText |取 label 的文字，回傳文字 |
+| getByLabelText | 取 label 的文字，回傳文字 |
 | getByPlaceholderText | 取 placeholder 的文字，回傳文字 |
 | getByText(/about/i) | 取文字 使用正規，回傳文字 |
 | getByAltText        | 取 alt 的文字，回傳文字 |
-| ByTitle             | 取 `<title/>` 文字 下面兩種都可以 |
+| ByTitle             | 取 `<title>文字</title>` 文字 or `<span title="文字"></span>`，回傳文字 |
 | ByDisplayValue      |                                 |
 | ByRole              |                                  |
 | ByTestId 常用       | 取 `<button data-testid="toggle"></button>` 回傳 tree  |
 
+範例
+
+```jsx
+// index.js
+
+<button data-testid="toggle">按鈕</button>
 ```
-toggle = getByTestId('toggle')
-toggle.textContent
+
+```jsx
+// page.test.js
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import Button from '../index'
+
+it('',()=>{
+    const {getByTestId} = render(<Button/>)
+    const toggle = getByTestId('btn-toggle')
+    expect(toggle.textContent).toBe('按鈕')
+})
 ```
+
--- src/button/__test__/index.test.js
+++ src/button/__test__/index.test.js
@@ -3,22 +3,9 @@ import { render, fireEvent } from '@testing-library/react'
 const { toMatchDiffSnapshot } = require('snapshot-diff');
 import Button from '../index'
 
-
-// getByLabelText             取 label 的文字re文字
-// getByPlaceholderText       取 placeholder 的文字，回傳文字
-// getByText(/about/i)        取文字 使用正規，回傳文字
-// getByAltText               取 alt 的文字，回傳文字
-// ByTitle                    取 <title/> 文字 下面兩種都可以
-// ByDisplayValue
-// ByRole
-// ByTestId 常用              取 <button data-testid="toggle"></button> 回傳 tree
-                             //  toggle = getByTestId('toggle')
-                             //  toggle.textContent
-
-
 describe('Button', () => {
     it('Queries', () => {
-        const { container, getByText, getByLabelText, getByPlaceholderText, getByAltText, getByDisplayValue, getByTestId } = render(<Button />)
+        const { container, getByTestId } = render(<Button />)
 
         // const inputNode = getByLabelText('username')
 
@@ -30,7 +17,7 @@ describe('Button', () => {
         // console.log(deleteElement)
 
         const toggle = getByTestId('btn-toggle')
-        console.log(toggle.textContent)
+        expect(toggle.textContent).toBe('按鈕')
 
     })
 })
