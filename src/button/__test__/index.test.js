8fcc73aec9c23bad999f3622e74f2fb0cc5e7ef0
RichardLiao <bnmghjtyu0@gmail.com>
2019-09-06 08:18:02 +0000

    update

--- src/button/__test__/index.test.js
+++ src/button/__test__/index.test.js
@@ -3,15 +3,34 @@ import { render, fireEvent } from '@testing-library/react'
 const { toMatchDiffSnapshot } = require('snapshot-diff');
 import Button from '../index'
 
-describe('Button', () => {
-    it('adf', () => {
-        const { container, debug, getByTestId, asFragment } = render(<Button />)
-        // console.log(debug())
-        const btnToggle = getByTestId('btn-toggle')
-        const content = getByTestId('content')
 
-        fireEvent.click(btnToggle)
-        expect(content.textContent).toBe('clicked')
+// getByLabelText             取 label 的文字re文字
+// getByPlaceholderText       取 placeholder 的文字，回傳文字
+// getByText(/about/i)        取文字 使用正規，回傳文字
+// getByAltText               取 alt 的文字，回傳文字
+// ByTitle                    取 <title/> 文字 下面兩種都可以
+// ByDisplayValue
+// ByRole
+// ByTestId 常用              取 <button data-testid="toggle"></button> 回傳 tree
+                             //  toggle = getByTestId('toggle')
+                             //  toggle.textContent
+
+
+describe('Button', () => {
+    it('Queries', () => {
+        const { container, getByText, getByLabelText, getByPlaceholderText, getByAltText, getByDisplayValue, getByTestId } = render(<Button />)
+
+        // const inputNode = getByLabelText('username')
+
+        // const inputNode = getByPlaceholderText('Username')
+
+        // const aboutAnchorNode = getByText(/about/i)
+
+        // const deleteElement = getByTitle('Delete')
+        // console.log(deleteElement)
+
+        const toggle = getByTestId('btn-toggle')
+        console.log(toggle.textContent)
 
     })
 })
--- src/button/index.js
+++ src/button/index.js
@@ -6,6 +6,16 @@ const Button = () => {
     }
     return (
         <div>
+            <label htmlFor="username-input">Username</label>
+            <input id="username-input" />
+            <input placeholder="Username" />
+            <a href="/about">About ℹ️</a>
+
+            <img alt="Incredibles 2 Poster" src="/incredibles-2.png" />
+
+            <title>Close</title>
+
+
             <button data-testid="btn-toggle" onClick={toggle}>按鈕</button>
             <div data-testid="content">{isClick ? 'clicked' : 'unclick'}</div>
         </div>
