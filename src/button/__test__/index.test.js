0a98725077df486aa6a4102c3cbf80e3e0205b54
RichardLiao <bnmghjtyu0@gmail.com>
2019-09-06 09:34:52 +0000

    test

--- src/button/__test__/index.test.js
+++ src/button/__test__/index.test.js
@@ -6,14 +6,7 @@ import Button from '../index'
 describe('Button', () => {
     it('adf', () => {
         const { container, debug, getByTestId, asFragment } = render(<Button />)
-        // console.log(debug())
-        const btnToggle = getByTestId('btn-toggle')
-        const btnSendData = getByTestId('sendData')
-        const content = getByTestId('content')
-        // fireEvent.click(domNode)
-        // fireEvent.click(btnToggle)
-        fireEvent.click(btnSendData)
-        // expect(content.textContent).toBe('clicked')
+
 
     })
 })
