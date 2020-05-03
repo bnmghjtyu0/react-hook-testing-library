3b658907c4cf9c7cf6477b32c50f9b7db3362e63
RichardLiao <bnmghjtyu0@gmail.com>
2020-05-03 20:51:25 +0800

    更新 react and test

--- src/button/__test__/index.test.js
+++ src/button/__test__/index.test.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-const { toMatchDiffSnapshot } = require('snapshot-diff');
-import Button from '../index'
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+const { toMatchDiffSnapshot } = require("snapshot-diff");
+import Button from "../index";
 
-describe('Button', () => {
-    it('adf', () => {
-        const { container, debug, getByTestId, asFragment } = render(<Button />)
-        // console.log(debug())
-        const btnToggle = getByTestId('btn-toggle')
-        const content = getByTestId('content')
+describe("Button", () => {
+  it("adf", () => {
+    const { container, debug, getByTestId, asFragment } = render(<Button />);
+    // console.log(debug())
+    const btnToggle = getByTestId("btn-toggle");
+    const content = getByTestId("content");
 
-        fireEvent.click(btnToggle)
-        expect(content.textContent).toBe('clicked')
-
-    })
-})
+    fireEvent.click(btnToggle);
+    expect(content.textContent).toBe("clicked");
+  });
+});
